237b64eea021a13baff4cf27dfb0738b8f9f2e97
Tim Dobson <hello@tdobson.net>
2024-09-18 09:55:26 +0100

    fix: Wrap column names with backticks in reportMembersToProcess.js

--- reportMembersToProcess.js
+++ reportMembersToProcess.js
@@ -6,14 +6,14 @@ function reportMembersToProcess(stmt) {
         first_name AS "Forenames",                                                                                                                                                                                                
         last_name AS "Surname",                                                                                                                                                                                                   
         "" AS "Previous Name",                                                                                                                                                                                                    
-        admin-bca-number AS "Membership Number",                                                                                                                                                                                  
-        (SELECT (CASE WHEN admin-other-club-name<>"" THEN admin-other-club-name ELSE "The Caving Crew" END)) AS "Primary Club Name",                                                                                              
+        \`admin-bca-number\` AS "Membership Number",                                                                                                                                                                                  
+        (SELECT (CASE WHEN \`admin-other-club-name\`<>"" THEN \`admin-other-club-name\` ELSE "The Caving Crew" END)) AS "Primary Club Name",                                                                                              
         membership_joining_date AS "Joining Date",                                                                                                                                                                                
-        (SELECT (CASE WHEN admin-other-club-name<>"" THEN "£0" ELSE "£20" END)) AS "Fee Paid",                                                                                                                                    
-        (SELECT (CASE WHEN admin-other-club-name<>"" THEN "AN" ELSE "C" END)) AS "Insurance Status",                                                                                                                              
+        (SELECT (CASE WHEN \`admin-other-club-name\`<>"" THEN "£0" ELSE "£20" END)) AS "Fee Paid",                                                                                                                                    
+        (SELECT (CASE WHEN \`admin-other-club-name\`<>"" THEN "AN" ELSE "C" END)) AS "Insurance Status",                                                                                                                              
         user_email AS "Email",                                                                                                                                                                                                    
-        admin-personal-pronouns AS "Gender",                                                                                                                                                                                      
-        admin-personal-year-of-birth AS "Year Of Birth",                                                                                                                                                                          
+        \`admin-personal-pronouns\` AS "Gender",                                                                                                                                                                                      
+        \`admin-personal-year-of-birth\` AS "Year Of Birth",                                                                                                                                                                          
         billing_address_1 AS "Address 1",                                                                                                                                                                                         
         billing_address_2 AS "Address 2",                                                                                                                                                                                         
         " ",                                                                                                                                                                                                                      
@@ -26,7 +26,7 @@ function reportMembersToProcess(stmt) {
       FROM jtl_member_db                                                                                                                                                                                                          
       LEFT JOIN jtl_order_product_customer_lookup pd ON pd.user_id = jtl_member_db.id                                                                                                                                             
       WHERE cc_member="yes" AND \`admin-bca-number\` IS NULL                                                                                                                                                                          
-      ORDER BY STR_TO_DATE(membership_joining_date, "%d/%m/%Y") ASC, admin-bca-number ASC, first_name ASC                                                                                                                         
+      ORDER BY STR_TO_DATE(membership_joining_date, "%d/%m/%Y") ASC, \`admin-bca-number\` ASC, first_name ASC                                                                                                                         
     `,
     formatting: [
       { type: 'wrap', column: "Address 1" },
